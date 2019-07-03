1cc9ca3611124c5bb6f34b39719ff2d29d8a9214
iris-the-fox <iris-miz@yandex.ru>
2019-07-03 14:10:45 +0300

    implement color to message

--- module1-solution/javascript/app.js
+++ module1-solution/javascript/app.js
@@ -7,20 +7,20 @@
   
   function LunchCheckController($scope) {
     $scope.lunch = "";
- 
+    $scope.color = "green"
     $scope.checkIt = function () {
       var lunshList = $scope.lunch.split(','); 
       $scope.len = lunshList.length;  
       if ($scope.lunch=="") {
           $scope.message =  "Please enter data first";
+          $scope.color = "red"
     	} 
       else if (lunshList.length>3){
       	$scope.message =  "Too much!";
       }
       else {
     		$scope.message = "Enjoy!";
-
-    	};
+      };
 
     };
 
